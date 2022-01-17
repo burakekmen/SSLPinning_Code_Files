5dbdd8fbcf687cbcd20f155d0af5fd9cec876ccc
Burak EKMEN <ekmen.burak@hotmail.com>
2022-01-17 10:14:47 +0300

    edit

--- NetworkSession.swift
+++ NetworkSession.swift
@@ -4,14 +4,11 @@ import TrustKit
  
 class NetworkSession: SessionDelegate {
  
- static let sharedSession = Session(delegate: NetworkSession())
- 
- override func urlSession(_ session: URLSession,
- task: URLSessionTask,
- didReceive challenge: URLAuthenticationChallenge,
- completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
- if TrustKit.sharedInstance().pinningValidator.handle(challenge, completionHandler: completionHandler) == false {
- completionHandler(.cancelAuthenticationChallenge, nil)
- }
- }
+static let sharedSession = Session(delegate: NetworkSession())
+    
+    override func urlSession(_ session: URLSession, task: URLSessionTask, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if TrustKit.sharedInstance().pinningValidator.handle(challenge, completionHandler: completionHandler) == false {
+                    completionHandler(.cancelAuthenticationChallenge, nil)
+                }
+    }
 }
